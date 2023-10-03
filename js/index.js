86b3809a3d361008777192df1246a534295b2fc4
MarcoTulioFonsecaAlves <65983912+MarcoTulioFonsecaAlves@users.noreply.github.com>
2023-10-03 09:30:40 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,9 @@
+/* navbar */
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "550px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
